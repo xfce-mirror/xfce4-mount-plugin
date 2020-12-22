1c0089524e158b7e77650471ab9b0b73c5857823
Fabian <timystery@arcor.de>
2020-12-23 00:52:47 +0100

    Comment out step by step old AC_CHECKS, explaining the reasoning for future reference and discussion, esp. wrt. *NIX compilation problems.

--- panel-plugin/devices.c
+++ panel-plugin/devices.c
@@ -32,7 +32,6 @@ Foundation, Inc., 675 Mass Ave, Cambridge, MA 02139, USA.
 #include <string.h>
 /* WEXITSTATUS */
 #include <sys/types.h>
-#include <sys/wait.h>
 #ifdef HAVE_GETMNTENT
 #include <mntent.h>
 #include <sys/vfs.h>
