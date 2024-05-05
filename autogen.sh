845fe986df4b93887fa3f822b2ee29bd13779fb1
Gaël Bonithon <gael@xfce.org>
2024-05-05 22:50:49 +0200

    build: Switch from intltool to gettext
    Related: xfce/xfce4-dev-tools#41

--- autogen.sh
+++ autogen.sh
@@ -1,23 +1,13 @@
 #!/bin/sh
-#
-# $Id: autogen.sh 830 2005-10-11 11:30:27Z timystery $
-#
-# Copyright (c) 2002-2005
-#         The Xfce development team. All rights reserved.
-#
-# Written for Xfce by Benedikt Meurer <benny@xfce.org>.
-#
 
-(type xdt-autogen) >/dev/null 2>&1 || {
+type xdt-autogen >/dev/null 2>&1 || {
   cat >&2 <<EOF
 autogen.sh: You don't seem to have the Xfce development tools installed on
             your system, which are required to build this software.
             Please install the xfce4-dev-tools package first, it is available
-            from https://www.xfce.org/.
+            from your distribution or https://www.xfce.org/.
 EOF
   exit 1
 }
 
-exec xdt-autogen $@
-
-# vi:set ts=2 sw=2 et ai:
+XDT_AUTOGEN_REQUIRED_VERSION="4.17.1" exec xdt-autogen "$@"
